b8d13285f5ba85ebda2fb2fc7f8257beffc0f3b8
Javier Sanchez <javier.bravo@netexlearning.com>
2025-11-02 13:03:57 +0100

    Add initial configuration for service

---
+++ src/main/java/com/bcnc/prices/TestJavaApplication.java
@@ -0,0 +1,16 @@
+package com.bcnc.prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal de la aplicación Spring Boot.
+ */
+@SpringBootApplication
+public class TestJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestJavaApplication.class, args);
+  }
+
+}
